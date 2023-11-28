abc9c0f86ff9d02efb705109e8de1be118cf7a05
Shreyansh <Srkeshri.srk@gmail.com>
2023-11-28 18:08:40 +0530

    complete Credit Amount UI.

--- CRED/Modules/Expand & Collapse/Table Cell/CreditAmountTableViewCell.swift
+++ CRED/Modules/Expand & Collapse/Table Cell/CreditAmountTableViewCell.swift
@@ -10,6 +10,11 @@ import UIKit
 class CreditAmountTableViewCell: UITableViewCell {
     
     @IBOutlet weak var outerView: UIView!
+    @IBOutlet weak var sliderView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -21,6 +26,10 @@ class CreditAmountTableViewCell: UITableViewCell {
     private func setupUI() {
         outerView.layer.cornerRadius = 15.0
         outerView.layer.masksToBounds = true
+        
+        sliderView.layer.cornerRadius = sliderView.bounds.size.height / 2
+        sliderView.layer.borderWidth = 10.0
+        sliderView.layer.borderColor = UIColor.deer.cgColor
     }
 
     
