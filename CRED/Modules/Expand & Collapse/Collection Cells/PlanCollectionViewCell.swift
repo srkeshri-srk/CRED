d7e995715fb2af36da6d609a523fa7e431e224aa
Shreyansh <Srkeshri.srk@gmail.com>
2023-11-28 19:04:10 +0530

    complete Plan Collectionview UI.

--- CRED/Modules/Expand & Collapse/Collection Cells/PlanCollectionViewCell.swift
+++ CRED/Modules/Expand & Collapse/Collection Cells/PlanCollectionViewCell.swift
@@ -11,7 +11,10 @@ class PlanCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var planView: UIView!
     @IBOutlet weak var recommendedView: UIView!
-    @IBOutlet weak var planTopLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var selectionButton: UIButton!
+    @IBOutlet weak var seeCalculationsButton: UIButton!
     
     let colorPalettes: [UIColor] = [.charlestonGreen, .cola, .crayola, .deer, .jacarta, .policeBlue]
     
@@ -28,6 +31,25 @@ class PlanCollectionViewCell: UICollectionViewCell {
 
         recommendedView.layer.cornerRadius = recommendedView.bounds.size.height / 2
         recommendedView.layer.masksToBounds = true
+        recommendedView.isHidden = [true, false].randomElement() ?? false
+        
+        selectionButton.layer.cornerRadius = selectionButton.bounds.size.height / 2
+        selectionButton.layer.borderWidth = 1
+        selectionButton.layer.borderColor = UIColor.white.withAlphaComponent(0.5).cgColor
     }
-
+    
+    @IBAction func selectionButtonAction(_ sender: UIButton) {
+        selectionButton.isSelected = !selectionButton.isSelected
+        
+        if selectionButton.isSelected {
+            selectionButton.setImage(UIImage(systemName: "checkmark"), for: .normal)
+        } else {
+            selectionButton.setImage(nil, for: .normal)
+        }
+    }
+    
+    @IBAction func seeCalculationsButtonAction(_ sender: UIButton) {
+        //Handle for See calculations
+    }
+    
 }
