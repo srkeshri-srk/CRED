dffb41549fb965566ef1e7e5b62927e654b02dbc
Shreyansh <Srkeshri.srk@gmail.com>
2023-11-28 15:38:04 +0530

    Done UI of Home VC.

--- CRED/Modules/Home/HomeViewController.swift
+++ CRED/Modules/Home/HomeViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class HomeViewController: BaseViewController {
     
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var ctaButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,6 +20,17 @@ class HomeViewController: BaseViewController {
     
     private func setupUI() {
         setNavBar(title: Constants.Home.title)
+        
+        ctaButton.layer.cornerRadius = 12.0
+        ctaButton.layer.masksToBounds = true
+        ctaButton.backgroundColor = .white
+        ctaButton.tintColor = .vampireBlack
+        descriptionLabel.textColor = .white
     }
+    
+    @IBAction func ctaButtonAction(_ sender: UIButton) {
+        print("Processing...")
+    }
+    
     
 }
