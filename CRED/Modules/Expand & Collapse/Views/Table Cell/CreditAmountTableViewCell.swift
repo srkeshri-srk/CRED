1d5a8db4c0145bae55b9fc58ba5e7c0aad056639
Shreyansh <Srkeshri.srk@gmail.com>
2023-11-30 04:35:14 +0530

    Slider Work and UI Done.

--- CRED/Modules/Expand & Collapse/Views/Table Cell/CreditAmountTableViewCell.swift
+++ CRED/Modules/Expand & Collapse/Views/Table Cell/CreditAmountTableViewCell.swift
@@ -6,20 +6,28 @@
 //
 
 import UIKit
+import HGCircularSlider
+
+protocol CreditAmountable: AnyObject {
+    func sliderValueChanged(value: CGFloat)
+}
 
 class CreditAmountTableViewCell: UITableViewCell {
     
     @IBOutlet weak var outerView: UIView!
-    @IBOutlet weak var sliderView: UIView!
+    @IBOutlet weak var sliderView: CircularSlider!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var amountLabel: UILabel!
     @IBOutlet weak var subTitleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    weak var delegate: CreditAmountable?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
 
         setupUI()
+        setUpCircularView()
     }
 
     
@@ -27,10 +35,28 @@ class CreditAmountTableViewCell: UITableViewCell {
         outerView.layer.cornerRadius = 15.0
         outerView.layer.masksToBounds = true
         
-        sliderView.layer.cornerRadius = sliderView.bounds.size.height / 2
-        sliderView.layer.borderWidth = 10.0
-        sliderView.layer.borderColor = UIColor.deer.cgColor
+        sliderView.diskFillColor = .white
+        sliderView.diskColor = .white
+        sliderView.trackFillColor = .middleRed
+        sliderView.trackColor = .linen
+        sliderView.endThumbStrokeColor = .deepPeach
+        sliderView.endThumbTintColor = .blackCoffee
+        sliderView.endThumbStrokeHighlightedColor = .middleRed
+        sliderView.lineWidth = 14
+        sliderView.backtrackLineWidth = 14
+        sliderView.numberOfRounds = 1
+        sliderView.stopThumbAtMinMax = true
+    }
+    
+    func setUpCircularView(){
+        sliderView.minimumValue = 0
+        sliderView.maximumValue = 50000
+        sliderView.addTarget(self, action: #selector(sliderMoved), for: .valueChanged)
     }
 
-    
+    @objc func sliderMoved() {
+        let amount = sliderView.endPointValue
+        amountLabel.text = amount.formatted(.currency(code: "INR"))
+        delegate?.sliderValueChanged(value: sliderView.endPointValue)
+    }
 }
